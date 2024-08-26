0fe9f0464d16afa9aa7ec9f6f04168e10e535084
0xjihan <jihankhan966@gmail.com>
2024-08-26 09:33:04 +0600

    English Language Added

---
+++ app/src/main/java/com/jihan/shohid/fragment/SettingFragment.kt
@@ -0,0 +1,39 @@
+package com.jihan.shohid.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity.MODE_PRIVATE
+import androidx.fragment.app.Fragment
+import com.jihan.shohid.activity.MainActivity
+import com.jihan.shohid.databinding.FragmentSettingBinding
+
+class SettingFragment : Fragment() {
+
+    private lateinit var binding: FragmentSettingBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSettingBinding.inflate(inflater)
+
+
+        binding.button.setOnClickListener{
+            val editor = activity?.getSharedPreferences("MyPrefs", MODE_PRIVATE)?.edit()
+            editor?.putBoolean("isEnglish",true)
+            editor?.apply()
+
+        }
+
+        binding.button2.setOnClickListener{
+            val editor = activity?.getSharedPreferences("MyPrefs", MODE_PRIVATE)?.edit()
+            editor?.putBoolean("isEnglish",false)
+            editor?.apply()
+        }
+
+
+
+        return binding.root
+    }
+}
